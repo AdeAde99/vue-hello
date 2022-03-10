102526b0aad4e8fad55d6937025ea4ddb624dd3e
AdeAde99 <adelintanasa.d@gmail.com>
2022-03-10 19:12:29 +0100

    consegna esercizio

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,8 @@
-const app = new vue ({
+const app = new Vue ({
     el : '#app',
     data: {
-        
+        saluto: 'Hello World!',
+        immagine: 'https://picsum.photos/200/300',
+        altImmagine: 'Immagine in Vue'
     }
 });
